51adc743389d6935a459ad8d2751166991405c6c
rjshoemaker55 <rjshoemaker55@gmail.com>
2019-11-06 21:18:40 -0500

    onclick functins complete

--- src/App.js
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-
-import Fun from './components/Fun';
-const App = () => {
-  return <h1>Wecome to React</h1>;
-};
-
-export default App;
--- src/components/Fun.js
+++
@@ -1,3 +0,0 @@
-import React from 'react';
-
-const Fun = () => <h1>hello</h1>;
